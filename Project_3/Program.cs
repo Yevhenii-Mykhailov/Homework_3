a940c17757cdc5a47e5e54f63175be9bd9ff28ca
Yevhenii Mykhailovskyi <evg@MacBook-Air-Evgenij.local>
2022-07-02 14:09:00 +0300

    create project

---
+++ Project_3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
